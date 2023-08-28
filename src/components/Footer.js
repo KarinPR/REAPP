b64fcbf8c160a45d799c73ef41866a9b878144b4
Karin <karin.buynholdinvestments@gmail.com>
2023-08-28 11:04:27 +0300

    Added Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import { Container, Typography } from '@material-ui/core';
+
+function Footer() {
+  return (
+    <Container>
+      <Typography variant="body1">
+        © 2023 Real Estate Scroller
+      </Typography>
+    </Container>
+  );
+}
+
+export default Footer;
