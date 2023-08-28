309ea2f89cfcae32cfa72015ec3c3353b0cae471
Karin <karin.buynholdinvestments@gmail.com>
2023-08-28 11:03:12 +0300

    Added main content area component

---
+++ src/components/MainContent.js
@@ -0,0 +1,13 @@
+import { Container, Grid } from '@material-ui/core';
+
+function MainContent() {
+  return (
+    <Container>
+      <Grid container spacing={3}>
+        {/* Add grid items here */}
+      </Grid>
+    </Container>
+  );
+}
+
+export default MainContent;
